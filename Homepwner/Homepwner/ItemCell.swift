6c2e22c65acb22882b2f039a986dd0d66e1f113b
Hisham Abraham <hishamabraham@gmail.com>
2017-03-04 06:46:59 -0500

    End of chapter 12

---
+++ Homepwner/Homepwner/ItemCell.swift
@@ -0,0 +1,27 @@
+//
+//  ItemCell.swift
+//  Homepwner
+//
+//  Created by Hisham Abraham on 3/3/17.
+//  Copyright © 2017 Hisham Abraham. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+    
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var serialNumberLabel: UILabel!
+    @IBOutlet var valueLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        nameLabel.adjustsFontForContentSizeCategory = true
+        serialNumberLabel.adjustsFontForContentSizeCategory = true
+        valueLabel.adjustsFontForContentSizeCategory = true
+    }
+    
+
+    
+}
